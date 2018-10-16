76c84f2f91f43de46e649a4932cabbdf8123ba73
Robert Fitak <rfitak9@gmail.com>
2018-10-16 08:36:19 -0400

    Add files via upload

---
+++ Data/remove-mitoMP10k.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l
+# author: rfitak
+#SBATCH -J MP10k
+#SBATCH -o MP10krmMito.out
+#SBATCH -e MP10krmMito.err
+#SBATCH -p biodept
+#SBATCH -n 1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH -t 12:00:00
+#SBATCH --mem-per-cpu=8000
+#SBATCH --mail-type=END
+#SBATCH --mail-user=rfitak9@gmail.com
+
+cd /datacommons/netscratch/frr6/SHAD_GENOME
+name="MP10k"
+
+# Do Mapping for PE reads
+bowtie2 \
+   --phred33 \
+   -q \
+   --very-sensitive \
+   --minins 7000 \
+   --maxins 15000 \
+   --rf \
+   --threads 8 \
+   --reorder \
+   -x Asap_mito \
+   -1 ${name}_F.trimmed.uniq.unj.fq.gz \
+   -2 ${name}_R.trimmed.uniq.unj.fq.gz | \
+   samtools1.3 view -b -F 2 | \
+   samtools1.3 sort -T ${name}.tmp -n -O bam | \
+   bedtools bamtofastq -i - -fq ${name}_F.trimmed.uniq.unj.noMito.fq -fq2 ${name}_R.trimmed.uniq.unj.noMito.fq
+
+# Get Stats
+seqtk fqchk -q20 <(cat ${name}_*.trimmed.uniq.unj.noMito.fq) | head
+seqtk fqchk -q30 <(cat ${name}_*.trimmed.uniq.unj.noMito.fq) | head
+
+# Compress the resulting reads
+gzip ${name}_F.trimmed.uniq.unj.noMito.fq
+gzip ${name}_R.trimmed.uniq.unj.noMito.fq
+
